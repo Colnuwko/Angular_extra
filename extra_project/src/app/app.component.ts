16059dd106140161436d5921887c8a266b2d516d
DanilSukach <danil211-1@yandex.ru>
2024-05-06 23:44:04 +0400

    добавил плашку

--- extra_project/src/app/app.component.ts
+++ extra_project/src/app/app.component.ts
@@ -2,19 +2,20 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navigation/navbar/navbar.component';
 import { BrowserModule } from '@angular/platform-browser';
-import {ProductComponent} from "./card/product/product.component";
-import {PrCardInt} from "./interfeces";
+import { ProductComponent } from "./card/product/product.component";
+import { PrCardInt } from "./interfeces";
+import { ProductCardModuleComponent } from './product-card-module/product-card-module.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, RouterLink, ProductComponent],
+  imports: [RouterOutlet, NavbarComponent, RouterLink, ProductComponent, ProductCardModuleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'extra_project';
-  card: PrCardInt = {image: "sf,dfsdf", sale: "sf,dfsdf", name: "sf,dfsdf", price:"sf,dfsdf"};
+  card: PrCardInt = { image: "sf,dfsdf", sale: "sf,dfsdf", name: "sf,dfsdf", price: "sf,dfsdf" };
 }
