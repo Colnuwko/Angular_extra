a7118045ae2e0a69403dbf17f510155c18559ed8
DanilSukach <danil211-1@yandex.ru>
2024-05-07 17:43:54 +0400

    стилизовал фильтр

--- extra_project/src/app/filter/filter.component.ts
+++ extra_project/src/app/filter/filter.component.ts
@@ -11,35 +11,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
-  products = [
-    { name: 'Яблоко', price: 50, category: 'Фрукты', inStock: true },
-    { name: 'Молоко', price: 80, category: 'Молочные продукты', inStock: true },
-    { name: 'Хлеб', price: 30, category: 'Хлебобулочные изделия', inStock: false },
-    // Добавьте больше продуктов по аналогии
-  ];
+
 
   categories = ['Фрукты', 'Молочные продукты', 'Хлебобулочные изделия'];
 
-  minPrice!: number;
-  maxPrice!: number;
+  minPrice: number = 0;
+  maxPrice: number = 1000;
   selectedCategory!: string;
-  inStock!: boolean;
+  inStock: boolean = false;
+
 
   clearFilters() {
-    this.minPrice = 1000;
-    this.maxPrice = 0;
+    this.minPrice = 0;
+    this.maxPrice = 1000;
     this.selectedCategory = '';
     this.inStock = false;
   }
-  get filteredProducts() {
-    return this.products.filter(product => {
-      if ((this.minPrice && product.price < this.minPrice) ||
-        (this.maxPrice && product.price > this.maxPrice) ||
-        (this.selectedCategory && product.category !== this.selectedCategory) ||
-        (this.inStock && !product.inStock)) {
-        return false;
-      }
-      return true;
-    });
+
+  OnClick() {
+    alert();
   }
+
+
 }
