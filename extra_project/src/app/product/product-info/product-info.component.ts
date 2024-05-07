1230080953d65f4e20d1d9dd57931ae858ed2dfa
Владислав <107910988+Colnuwko@users.noreply.github.com>
2024-05-07 16:30:41 +0400

    Revert "Razzzenya branch"

--- extra_project/src/app/product/product-info/product-info.component.ts
+++ extra_project/src/app/product/product-info/product-info.component.ts
@@ -1,28 +1,12 @@
-import { Component, Input } from '@angular/core';
-import { PrCardInt } from '../../interfaces';
-import { ActivatedRoute } from '@angular/router';
-import { ServiceService } from '../../services/service.service';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-info',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './product-info.component.html',
   styleUrl: './product-info.component.css'
 })
 export class ProductInfoComponent {
-  @Input() product_id!: number
-  card!: PrCardInt[]
-  constructor(private route: ActivatedRoute, private productService: ServiceService) { }
-  ngOnInit(): void {
-    this.productService.getCardsByNumber([this.product_id]).subscribe(
-      (data: PrCardInt[]) => {
-        this.card = data;
-      },
-      (error) => {
-        console.error('Error fetching cards:', error);
-      }
-    );
-  }
+
 }
